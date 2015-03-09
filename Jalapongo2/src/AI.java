6164a6e61b2d4a2337456deade6ac924f28121d4
Dalton0022 <dalton.lheureux@yahoo.com>
2015-03-09 14:58:30 -0400

    Added AI Class

---
+++ Jalapongo2/src/AI.java
@@ -0,0 +1,14 @@
+
+public class AI extends Player{
+
+	public AI(int pos) {
+		super(pos);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
