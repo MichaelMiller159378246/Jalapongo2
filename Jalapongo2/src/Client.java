a5e824f24c4e80858074d983f57300fc6cb7e5c7
Leslie Reis <Reisl@my.erau.edu>
2015-03-10 17:50:47 -0400

    Updated client code

--- Jalapongo2/src/Client.java
+++ Jalapongo2/src/Client.java
@@ -3,17 +3,25 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.io.IOException;
 
 public class Client{
 	private static Socket socket;
+	
 	public static int port;
 	private BufferedReader in;
 	private PrintWriter out;
 	
+
 	//Construct client 
 	public Client(String ipAddress) throws Exception{
-		//Setup networking
-		socket = new Socket(ipAddress,port);
+		
 	}
 	
-}
+	//connect to server and enter processing loop
+	private void run() throws IOException{
+		
+		//Make connection
+	
+	}
+}//end Client
