0f00d800c65e6bb9aee30d0dd9b0c53c895fdce0
Leslie Reis <Reisl@my.erau.edu>
2015-03-10 18:55:22 -0400

    Added client communication with server to verify username.

--- Jalapongo2/src/Client.java
+++ Jalapongo2/src/Client.java
@@ -1,4 +1,4 @@
-//import
+/*import*/
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -7,21 +7,47 @@ import java.io.IOException;
 
 public class Client{
 	private static Socket socket;
-	
-	public static int port;
 	private BufferedReader in;
 	private PrintWriter out;
+	private static int port;
+	private static String ipAddress;
+	private static String name;
 	
-
-	//Construct client 
-	public Client(String ipAddress) throws Exception{
-		
+	/*Construct client*/
+	public Client(int portN, String IP,String uName) {
+		port = portN;
+		ipAddress = IP;
+		name = uName;
 	}
 	
-	//connect to server and enter processing loop
+	private String getName(){
+		return name;
+	}
+	
+	/*connect to server and enter processing loop*/
 	private void run() throws IOException{
 		
-		//Make connection
+		//Make connection and initialize streams
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		out = new PrintWriter(socket.getOutputStream(),true);
+		
+		
+		/*Submit name and get verification that it is accepted*/
+		while(true){
+			String line = in.readLine();
+			if(line.startsWith("SUBMITNAME")){
+				out.println(getName());
+			}
+			else if (line.startsWith("NAMEACCEPTED")){
+				
+			}
+		}//end while loop
+	}//end run
 	
+	/*run the client*/
+	public static void main(String[] args) throws Exception{
+		Client user = new Client(port,ipAddress,name);
+		user.run();
+		
 	}
 }//end Client
