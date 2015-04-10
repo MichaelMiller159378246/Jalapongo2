d8139c6726a87d3ef37279eb8551f50f42861928
jonpaulhemi <jonpaulhemi@gmail.com>
2015-04-10 15:19:55 -0400

    Fixed the ball tracking algorithm to work better with large and small paddles

--- Jalapongo2/src/AI.java
+++ Jalapongo2/src/AI.java
@@ -11,13 +11,13 @@ public class AI extends Player{
 		if (paddle.getLives() > 0 && paddle.getControls() != 0) {
 		
 		if (this.pos == 1 || this.pos == 3) {
-			if ((ball.getYLoc() - this.getPaddle().getLength()/2) > this.getLoc())
+			if ((ball.getYLoc() - this.getPaddle().getPaddle().getHeight()/2) > this.getLoc())
 				paddle.paddleMove(1);
 			else
 				paddle.paddleMove(-1);
 		}
 		else {
-			if ((ball.getXLoc() - this.getPaddle().getLength()/2) > this.getLoc())
+			if ((ball.getXLoc() - this.getPaddle().getPaddle().getWidth()/2) > this.getLoc())
 				paddle.paddleMove(1);
 			else
 				paddle.paddleMove(-1);
