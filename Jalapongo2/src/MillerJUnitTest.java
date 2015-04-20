a4e622005e7eefdf227bdcb9b97c102e70219530
MichaelMiller159378246 <millem62@my.erau.edu>
2015-04-20 14:55:42 -0400

    Commit

--- Jalapongo2/src/MillerJUnitTest.java
+++ Jalapongo2/src/MillerJUnitTest.java
@@ -1,5 +1,4 @@
-import static org.junit.Assert.*;
-import javafx.application.Platform;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -7,9 +6,13 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class MillerJUnitTest {
 
+public class MillerJUnitTest extends Application implements Runnable{
+	
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		System.out.println("setUpBeforeClass: Preparing to run unit test(s)...\n");
@@ -31,7 +34,19 @@ public class MillerJUnitTest {
 	}
 
 	@Test
-	public void test() {
+	public void musicTest() {
+		// Was unable to get it to pass this part as the JUnit is unable to run while GUI is running
 		assertTrue(GameGUI.musicCB.isSelected());
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		GameGUI game = new GameGUI();
+		game.start(arg0);
+	}
+
+	@Override
+	public void run() {
+		GameGUI.launch();
 	}
 }
